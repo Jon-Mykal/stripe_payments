81e165684831abbb31a6ccf4bddf67a79a1c15a3
Gregg Wong <gwong@smartterm.io>
2023-02-21 22:51:42 -0500

    Update environment variables with stripe key

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
         messagingSenderId: "600388385735",
         appId: "1:600388385735:web:bb67a9ba8419f3456ef7e2",
         measurementId: "G-KL7W7BK1EV"
-    }
+    },
+    stripeKey: "pk_test_51K4WXUGIjFMC5qDvLBovqTIsYPwsZZ7rinFTEL1UQ798Y96xDCAjYESubXsknFKVutUyFIW3H5IWrWIHDiGsemuT00A72ii7Yd"
   };
