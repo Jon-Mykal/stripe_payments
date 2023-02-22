4ea5c5c15286187b7e949aa2ffd375075cdc5a98
Gregg Wong <gwong@smartterm.io>
2023-02-22 18:51:39 -0500

    Add Checkout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { loadStripe, Stripe } from '@stripe/stripe-js';
+import { HttpClient } from '@angular/common/http';
+import { StripeService } from 'ngx-stripe';
+import { switchMap } from 'rxjs';
+import { environment } from './environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stripe_payments';
+  baseUrl = environment.apiUrl;
+  
+  constructor(
+    private stripeService: StripeService,
+    private http: HttpClient) {
+    console.log('AppComponent.constructor()');
+    
+  }
+
+  checkout() {
+    
+    // Check the server.js tab to see an example implementation
+    this.http.post(`${this.baseUrl}/create-checkout-session`, {})
+      .pipe(
+        switchMap((session: any) => {
+          return this.stripeService.redirectToCheckout({ sessionId: session["id"] })
+        })
+      )
+      .subscribe(result => {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, you should display the localized error message to your
+        // customer using `error.message`.
+        if (result.error) {
+          alert(result.error.message);
+        }
+      });
+  }
 }
