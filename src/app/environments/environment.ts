d4328b04edcb139c3d1ab3e3fb70a751cf78f1b1
Gregg Wong <gwong@smartterm.io>
2023-02-21 22:32:29 -0500

    Add firebase config

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBeq14K-MPGrCmmhLGn-2V2oMlfxt5koCE",
+        authDomain: "fire-money-18f9d.firebaseapp.com",
+        projectId: "fire-money-18f9d",
+        storageBucket: "fire-money-18f9d.appspot.com",
+        messagingSenderId: "600388385735",
+        appId: "1:600388385735:web:bb67a9ba8419f3456ef7e2",
+        measurementId: "G-KL7W7BK1EV"
+    }
+  };
