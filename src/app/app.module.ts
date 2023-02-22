e94fca0b31688cddeba0806523d48d4d9672aa3f
Gregg Wong <gwong@smartterm.io>
2023-02-22 18:34:13 -0500

    Add HTTPClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgxStripeModule } from 'ngx-stripe';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { environment } from './environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxStripeModule.forRoot(environment.stripePublishableKey)
   ],
   providers: [],
   bootstrap: [AppComponent]
